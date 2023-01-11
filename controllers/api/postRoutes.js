cdb83d778b8784326ebd9fe44800f48bf584ee3d
Kay <ckay_006@yahoo.com>
2023-01-10 23:16:13 -0500

    user is now able to update a blog post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,66 +1,61 @@
-const router = require ('express').Router();
-const{Post, User, Comments} = require('../../models');
-const checkAuth = require ('../../utils/auth');
+const router = require('express').Router();
+const { Post, User, Comments } = require('../../models');
+const checkAuth = require('../../utils/auth');
 
 router.post('/newpost', async (req, res) => {
-    try {
-      const addPost = await Post.create({
-      title: req.body.title,
-      content: req.body.content,
-      user_id: req.session.user_id,
-      }
-      );
-  
-       req.session.save(() => {
-        req.session.logged_in = true;
-        res.status(200).json(addPost);
-      });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json(error);
-    }
-  })
-
-
-router.put('/:id', checkAuth, async (req,res) => {
-    // update a post by its `id` value
   try {
-  const postInfo = await Post.update (
-    {
+    const addPost = await Post.create({
       title: req.body.title,
       content: req.body.content,
       user_id: req.session.user_id,
-  },
+    }
+    );
 
-{
-  where:{
-    id:req.params.id
-  },
-});
-
-req.session.save(() => {
-  req.session.logged_in = true;
-  res.status(200).json(postInfo);
-});
+    res.status(200).json(addPost);
 
   } catch (error) {
     console.log(error)
     res.status(500).json(error);
   }
-  })
+})
+
+
+router.put('/:id', checkAuth, async (req, res) => {
+  // update a post by its `id` value
+  try {
+    const postInfo = await Post.update(
   
-  router.delete('/:id', checkAuth, async (req,res) => {
-    // update a comment by its `id` value
-    Post.destroy ({
-    where:{
-      id:req.params.id
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+
+      {
+        where: {
+          id: req.params.id
+        },
+      });
+
+      res.status(200).json(postInfo);
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error);
+  }
+
+})
+
+router.delete('/:id', checkAuth, async (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
     },
   }
   )
-  .then ((deletedPost) => {
-    res.json(deletedPost)
-  })
-    .catch((err)=>res.json(err));
-  });
+    .then((deletedPost) => {
+      res.json(deletedPost)
+    })
+    .catch((err) => res.json(err));
+});
 
-  module.exports = router;
+module.exports = router;
