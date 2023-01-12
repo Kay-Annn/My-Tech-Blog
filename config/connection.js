b368ff858079f4313268af27ac86983a697e99bf
Kay <ckay_006@yahoo.com>
2023-01-12 11:38:55 -0500

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-
 if (process.env.JAWSDB_URL) {//Heroku
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {//Localhost
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-  }
-);
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql'
+    }
+  );
 }
 
 module.exports = sequelize;
